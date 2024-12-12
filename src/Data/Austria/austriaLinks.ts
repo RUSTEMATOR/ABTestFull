c46dfd22819ce5ccce7030b7ce917ffa01b266d3
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-12-12 19:12:11 +0200

    started AT

--- src/Data/Austria/austriaLinks.ts
+++ src/Data/Austria/austriaLinks.ts
@@ -4,18 +4,18 @@ interface ILinks {
 }
 
 export const AUSTRIA_LINKS_WELCOME: ILinks = {
-    Btag: 'https://ia.kbtraf.com/C.ashx?btag=a_2401b_1173c_&affid=17&siteid=2401&adid=1173&c=',
-    Stag: 'https://refer.kingtraf.com/a5a053d8f'
+    Btag: 'https://ia.kbtraf.com/C.ashx?btag=a_2401b_1211c_&affid=17&siteid=2401&adid=1211&c=',
+    Stag: 'https://refer.kingtraf.com/a418723cb'
 }
 
 export const AUSTRIA_LINKNDB: ILinks = {
-    Stag: 'https://refer.kingtraf.com/a499163a2', 
-    Btag: 'https://ia.kbtraf.com/C.ashx?btag=a_2401b_1460c_&affid=17&siteid=2401&adid=1460&c='
+    Stag: 'https://refer.kingtraf.com/a7ebde21b', 
+    Btag: 'https://ia.kbtraf.com/C.ashx?btag=a_2401b_1501c_&affid=17&siteid=2401&adid=1501&c='
 } 
 
 export const AT_STAGE_LINKS = {
     welcomeBtag: 'https://www.kingbillycasino1.com/land/at/kings_welcome_pack?btag=a_2401b_1170c_&nofingerprint=1', 
-    welcomeStag: 'https://www.kingbillycasino1.com/land/en-AU/kings_welcome_pack?stag=221080_66fbd980c308a07[%E2%80%A6]fer.kingtraf.com%2Fa020bc42f&__layerref=&nofingerprint=1',
-    NDBbtag: 'https://www.kingbillycasino1.com/land/en-AU/ndb?btag=a_2401b_1170c_&nofingerprint=1',
-    NDBstag: 'https://www.kingbillycasino1.com/land/en-AU/ndb?stag=221080_66fbd980c308a07[%E2%80%A6]fer.kingtraf.com%2Fa020bc42f&__layerref=&nofingerprint=1'
+    welcomeStag: 'https://www.kingbillycasino1.com/land/at/kings_welcome_pack?stag=221080_66fbd980c308a07[%E2%80%A6]fer.kingtraf.com%2Fa020bc42f&__layerref=&nofingerprint=1',
+    NDBbtag: 'https://www.kingbillycasino1.com/land/at/ndb?btag=a_2401b_1170c_&nofingerprint=1',
+    NDBstag: 'https://www.kingbillycasino1.com/land/at/ndb?stag=221080_66fbd980c308a07[%E2%80%A6]fer.kingtraf.com%2Fa020bc42f&__layerref=&nofingerprint=1'
 }
--- src/Data/Austria/expectedAustriaLinks.ts
+++ src/Data/Austria/expectedAustriaLinks.ts
@@ -6,30 +6,30 @@ interface IExpectedLinksNDB {
 
 interface IExpectedLinksWelcome {
     Welcome: string
-    World: string
+    Land: string
 }
 
 export const EXPECTED_AUSTRIA_WELCOME_LINKS: IExpectedLinksWelcome = {
     Welcome: 'https://www.kingbillywin20.com/land/en-AU/kings_welcome_pack_1',
-    World: 'https://www.kingbillywin20.com/land/en-AU/kings_world_welcome_pack_1'
+    Land: 'https://www.kingbillywin20.com/land/en-AU/kings_land_1'
 }
 
 
 export const EXPECTED_AUSTRIA_NDB_LINKS: IExpectedLinksNDB = {
-    expectedUrlNDB: 'https://www.kingbillywin20.com/land/en-AU/ndb_3',
-    expectedUrlNoDep: 'https://www.kingbillywin20.com/land/en-AU/kings_no_dep_3'
+    expectedUrlNDB: 'https://www.kingbillywin20.com/land/en-AU/ndb_2',
+    expectedUrlNoDep: 'https://www.kingbillywin20.com/land/en-AU/kings_no_dep_2'
 
 }
 
 
 export const EXPECTED_AUSTRIA_STAGE_WELCOME_LINKS: IExpectedLinksWelcome = {
     Welcome: 'https://www.kingbillycasino1.com/land/en-AU/kings_welcome_pack_1',
-    World: 'https://www.kingbillycasino1.com/land/en-AU/kings_world_welcome_pack_1'
+    Land: 'https://www.kingbillycasino1.com/land/en-AU/kings_land_1'
 }
 
 
  export const EXPECTED_AUSTRIA_STAGE_NDB_LINKS: IExpectedLinksNDB = {
-    expectedUrlNDB: 'https://www.kingbillycasino1.com/land/en-AU/ndb_3',
-    expectedUrlNoDep: 'https://www.kingbillycasino1.com/land/en-AU/kings_no_dep_3'
+    expectedUrlNDB: 'https://www.kingbillycasino1.com/land/en-AU/ndb_2',
+    expectedUrlNoDep: 'https://www.kingbillycasino1.com/land/en-AU/kings_no_dep_2'
 }
 
